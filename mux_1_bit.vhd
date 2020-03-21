76e57ec56587a81dc437de2cf277046faf65bc66
onclepixou <pfiliol96@orange.fr>
2020-03-21 11:44:38 +0100

    modified mux

--- mux_1_bit.vhd
+++ mux_1_bit.vhd
@@ -1,21 +1,32 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY mux_1_bit IS
+
     GENERIC(
-        N: INTEGER := 32);
-    PORT( sel:      in   STD_LOGIC;
-          e1:       in   STD_LOGIC_VECTOR(N - 1 downto 0);
-          e2:       in   STD_LOGIC_VECTOR(N - 1 downto 0);
-          s:        out  STD_LOGIC_VECTOR(N - 1 downto 0));
+
+        N: INTEGER := 32
+
+    );
+
+    PORT(
+
+        sel:      IN   STD_LOGIC;
+        e1:       IN   STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+        e2:       IN   STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+        s:        OUT  STD_LOGIC_VECTOR(N - 1 DOWNTO 0)
+        
+    );
+
 END;
 
-ARCHITECTURE behave OF mux_1_bit IS
+ARCHITECTURE arch OF mux_1_bit IS
 
-BEGIN
-    s <= e1 WHEN sel = '0' ELSE
-         e2 WHEN sel = '1';
-END;
+    BEGIN
+
+        s <= e1 WHEN sel = '0' ELSE
+             e2 WHEN sel = '1' ;
+
+END ARCHITECTURE;
     
