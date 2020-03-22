2b1c46f6ad263d4da65d4655e3cdde3c87c30de1
Golderfox <50454252+Golderfox@users.noreply.github.com>
2020-03-22 11:41:42 +0100

    Add an asynchroneous reset and valid address test

--- data_memory.vhd
+++ data_memory.vhd
@@ -8,13 +8,14 @@ ENTITY data_memory IS
     GENERIC(
           N: INTEGER := 32);
           
-    PORT( clk, we:      IN    STD_LOGIC;
+    PORT( clk, we, rst: IN    STD_LOGIC;
           a, wd:        IN    STD_LOGIC_VECTOR(N - 1 downto 0);
           rd:           OUT   STD_LOGIC_VECTOR(N - 1 downto 0));
 END;
 
 ARCHITECTURE behave OF data_memory IS
 
+CONSTANT wrong_address: STD_LOGIC_VECTOR(N - 1 DOWNTO 0) := (OTHERS => 'U');
 TYPE ramtype IS ARRAY (2**N - 1 DOWNTO 0) OF STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
 SIGNAL mem: ramtype;
 
@@ -25,27 +26,45 @@ BEGIN
 
         BEGIN
         
-            -- memory initialisation to 0
+            -- initialisation memory set to 0
             FOR i IN 0 TO N - 1 LOOP
             
                 mem(i) <= (OTHERS => '0');
                 
             END LOOP;
             
-            -- read and write
+            -- read and write loop
             LOOP
-            
-                IF (clk'EVENT AND clk = '1') THEN
+                -- asynchronous rest
+                IF (rst = '1') THEN
                 
-                    -- write affectation
-                    IF (we = '1') THEN
+                        FOR i IN 0 TO N - 1 LOOP
+            
+                            mem(i) <= (OTHERS => '0');
+                
+                        END LOOP;
+                   
+                -- rising edge
+                ELSIF (clk'EVENT AND clk = '1') THEN
                     
-                        mem(TO_INTEGER(UNSIGNED(a(N - 1 DOWNTO 2)))) <= wd;
+                    -- valid address test
+                    IF a = wrong_address THEN
+                    
+                        rd <= (OTHERS => 'U');
+                    
+                    ELSE
+                    
+                        -- write affectation
+                        IF (we = '1') THEN
+                    
+                            mem(TO_INTEGER(UNSIGNED(a(N - 1 DOWNTO 2)))) <= wd;
                         
-                    END IF;
+                        END IF;
                     
-                    -- read affectation
-                    rd <= mem(TO_INTEGER(UNSIGNED(a(N - 1 DOWNTO 2))));
+                        -- read affectation
+                        rd <= mem(TO_INTEGER(UNSIGNED(a(N - 1 DOWNTO 2))));
+
+                    END IF;
                     
                 END IF;
                 
