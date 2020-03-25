a75514641754c4625abe68e233560d754923cee1
onclepixou <pfiliol96@orange.fr>
2020-03-25 13:02:58 +0100

    alu without cout

--- alu.vhd
+++ alu.vhd
@@ -15,7 +15,6 @@ ENTITY alu is
            b          : IN  STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
            alucontrol : IN  STD_LOGIC_VECTOR(1 DOWNTO 0);
            result     : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-           cout       : OUT STD_LOGIC;
            aluflags   : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
     );
 
@@ -27,7 +26,7 @@ ARCHITECTURE arch OF alu IS
     SIGNAL sum           : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
     SIGNAL bmod          : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
     SIGNAL result_tmp    : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-    SIGNAL cout_buf      : STD_LOGIC;
+    SIGNAL cout      : STD_LOGIC;
     SIGNAL temp          : STD_LOGIC;
 
     COMPONENT N_bits_full_adder is
@@ -59,14 +58,13 @@ ARCHITECTURE arch OF alu IS
                       (a OR  b) WHEN alucontrol = "11";
 
         result <= result_tmp;
-        cout <= cout_buf;
-        
+    
         -- negative
         aluflags(3) <= result_tmp(N - 1);
         -- zero
         aluflags(2) <= '1' WHEN result_tmp = ZERO_VECTOR ELSE '0';
         -- carry
-        aluflags(1) <= (NOT(alucontrol(1))) AND cout_buf ;
+        aluflags(1) <= (NOT(alucontrol(1))) AND cout ;
         -- overflow
         aluflags(0) <= (NOT(alucontrol(1))) AND (a(N - 1) XOR sum(N - 1)) AND (alucontrol(0) XNOR a(N - 1) XNOR b(N - 1));
 
@@ -79,7 +77,7 @@ ARCHITECTURE arch OF alu IS
                 b    => bmod, 
                 cin  => alucontrol(0), 
                 s    => sum,
-                cout => cout_buf 
+                cout => cout 
             );
 
 END ARCHITECTURE;
