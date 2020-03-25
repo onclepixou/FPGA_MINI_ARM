842da02e7481f790940648da958fc1108b67bd64
onclepixou <pfiliol96@orange.fr>
2020-03-25 12:39:34 +0100

    fixed ports

--- reg_file.vhd
+++ reg_file.vhd
@@ -8,9 +8,9 @@ ENTITY reg_file IS
         
         clk  :  IN  STD_LOGIC;
         we3  :  IN  STD_LOGIC;
-        ra1  :  IN  STD_LOGIC_VECTOR( 3 DOWNTO 0);
-        ra2  :  IN  STD_LOGIC_VECTOR( 3 DOWNTO 0);
-        wa3  :  IN  STD_LOGIC_VECTOR( 3 DOWNTO 0);
+        a1  :  IN  STD_LOGIC_VECTOR( 3 DOWNTO 0);
+        a2  :  IN  STD_LOGIC_VECTOR( 3 DOWNTO 0);
+        a3  :  IN  STD_LOGIC_VECTOR( 3 DOWNTO 0);
         wd3  :  IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
         r15  :  IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
         rd1  :  OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
@@ -27,7 +27,7 @@ ARCHITECTURE arch OF reg_file IS
 
     BEGIN
         -- Writing process
-        PROCESS(clk)
+        PROCESS(clk, we3, a3)
 
             BEGIN
 
@@ -35,7 +35,7 @@ ARCHITECTURE arch OF reg_file IS
 
                     IF (we3 = '1') THEN
 
-                        mem(TO_INTEGER(UNSIGNED(wa3))) <= wd3;
+                        mem(TO_INTEGER(UNSIGNED(a3))) <= wd3;
 
                     END IF;
 
@@ -44,39 +44,39 @@ ARCHITECTURE arch OF reg_file IS
             END PROCESS;
 
         -- Reading process
-        PROCESS(clk, ra1, ra2, we3, wd3, r15)
+        PROCESS(clk, a1, a2, r15)
 
             BEGIN
 
                 IF (clk'EVENT AND clk = '1') THEN
 
                     -- rd1 affectation
-                    IF (TO_INTEGER(UNSIGNED(ra1)) = 15) THEN 
+                    IF (TO_INTEGER(UNSIGNED(a1)) = 15) THEN 
 
                         rd1 <= r15;
 
-                    ELSIF (ra1 = "UUUU") THEN 
+                    ELSIF (a1 = "UUUU") THEN 
                     
                         rd1 <= (OTHERS => 'U');
 
                     ELSE 
 
-                        rd1 <= mem(TO_INTEGER(UNSIGNED(ra1)));
+                        rd1 <= mem(TO_INTEGER(UNSIGNED(a1)));
 
                     END IF;
 
                     -- rd2 affectation
-                    IF (TO_INTEGER(UNSIGNED(ra2)) = 15) THEN 
+                    IF (TO_INTEGER(UNSIGNED(a2)) = 15) THEN 
                     
                         rd2 <= r15;
 
-                    ELSIF (ra2 = "UUUU") THEN 
+                    ELSIF (a2 = "UUUU") THEN 
                     
                         rd2 <= (OTHERS => 'U');
 
                     ELSE 
                     
-                        rd2 <= mem(TO_INTEGER(UNSIGNED(ra2)));
+                        rd2 <= mem(TO_INTEGER(UNSIGNED(a2)));
 
                     END IF;
 
